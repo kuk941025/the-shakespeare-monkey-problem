759868bcb25e4337571cf7d1848a4c7bfb8cd2a2
JunKyus <kuk941025@gmail.com>
2020-09-14 19:52:39 +0900

    Remove app.css index.css count.js

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,35 +1,35 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import makeStyles from '@material-ui/core/styles/makeStyles';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
-const Setting = () => {
-    const classes = useStyles();
-    return (
-        <div className={classes.settingRoot}>
-            <Typography className={classes.typo}>
-                Target Word
-            </Typography>
-            <div className={classes.textRoot}>
-                <TextField />
-                <Button color="primary" variant="contained">Update</Button>
-            </div>
-        </div>
-    )
-}
+const Setting = ({ handleChange, value = "", handleUpdate }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.settingRoot}>
+      <Typography className={classes.typo}>Target Word</Typography>
+      <div className={classes.textRoot}>
+        <TextField value={value} onChange={handleChange} />
+        <Button color="primary" variant="contained" onClick={handleUpdate}>
+          Update
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-const useStyles = makeStyles(theme => ({
-    settingRoot: {
-        padding: theme.spacing(2)
-    },
-    typo: {
-        fontWeight: '600'
-    },
-    textRoot: {
-        display: 'flex',
-        justifyContent: 'space-between'
-    }
+const useStyles = makeStyles((theme) => ({
+  settingRoot: {
+    padding: theme.spacing(2),
+  },
+  typo: {
+    fontWeight: "600",
+  },
+  textRoot: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
 }));
 
 export default Setting;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
-import Setting from '../components/Setting';
+import Setting from "../components/Setting";
 const App = () => {
+  const [target, setTarget] = useState("");
+
+  const handleUpdate = () => {
+    console.log(target);
+  };
   return (
     <div className="App">
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Setting />
-        </Grid>
-        <Grid item xs={6}>
-          asd2
-        </Grid>
-      </Grid>
+      <Setting
+        value={target}
+        handleChange={({ target: { value } }) => setTarget(value)}
+        handleUpdate={handleUpdate}
+      />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
